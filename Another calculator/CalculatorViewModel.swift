fdc2108d68b06f7bdd6c5a7aa05e04a6532aba31
HETNOOR <daffixipop@gmail.com>
2024-10-11 22:38:01 +0700

    add

---
+++ Another calculator/CalculatorViewModel.swift
@@ -0,0 +1,176 @@
+//
+//  CalculatorViewModel.swift
+//  Another calculator
+//
+//  Created by Максим Герасимов on 11.10.2024.
+//
+import Foundation
+
+enum CalculationError: Error {
+    case dividedByZero
+}
+
+enum Operation: String {
+    case add = "plus"
+    case subtract = "minus"
+    case multiply = "multiply"
+    case divide = "divide"
+    case remainder = "remainder"
+    
+    func calculate(_ number1: Double, _ number2: Double) throws -> Double {
+        switch self {
+        case .add:
+            return number1 + number2
+        case .subtract:
+            return number1 - number2
+        case .multiply:
+            return number1 * number2
+        case .divide:
+            if number2 == 0 {
+                throw CalculationError.dividedByZero
+            }
+            return number1 / number2
+        case .remainder:
+            if number2 == 0 {
+                throw CalculationError.dividedByZero
+            }
+            return number1.truncatingRemainder(dividingBy: number2)
+        }
+    }
+}
+
+enum CalculationHistoryItem {
+    case number(Double)
+    case operation(Operation)
+}
+
+
+
+class CalculatorViewModel {
+    
+    var onResultUpdated: ((String) -> Void)?
+    var onHistoryUpdated: ((String) -> Void)?
+    
+    private var calculationHistory: [CalculationHistoryItem] = []
+    private let numberFormatter: NumberFormatter
+    
+    init() {
+        numberFormatter = NumberFormatter()
+        numberFormatter.usesGroupingSeparator = false
+        numberFormatter.locale = Locale(identifier: "ru_RU")
+        numberFormatter.numberStyle = .decimal
+    }
+    
+    func handleButtonPress(_ buttonText: String, currentResultText: String?) {
+        guard var resultText = currentResultText else { return }
+        
+        if resultText == "Ошибка" {
+            resultText = "0"
+        }
+        
+        if buttonText == "," {
+            if resultText == "0" {
+                resultText = "0,"
+            } else if !resultText.contains(",") {
+                resultText.append(buttonText)
+            }
+        } else {
+            if resultText == "0" {
+                resultText = buttonText
+            } else {
+                resultText.append(buttonText)
+            }
+        }
+        
+        updateResult(resultText)
+    }
+    
+    func handleOperationPress(_ operationText: String, currentResultText: String?) {
+        guard
+            let buttonOperation = Operation(rawValue: operationText),
+            let resultText = currentResultText,
+            let labelNumber = numberFormatter.number(from: resultText)?.doubleValue
+        else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        calculationHistory.append(.operation(buttonOperation))
+        updateResult("0")
+    }
+    
+    func handleCalculatePress(currentResultText: String?) {
+        guard
+            let resultText = currentResultText,
+            let labelNumber = numberFormatter.number(from: resultText)?.doubleValue
+        else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        
+        do {
+            let resulted = try calculate()
+            if let formattedResult = numberFormatter.string(from: NSNumber(value: resulted)) {
+                updateResult(formattedResult)
+                updateHistory()
+            }
+        } catch {
+            updateResult("Ошибка")
+        }
+        
+        calculationHistory.removeAll()
+    }
+    
+    func handleClear() {
+        calculationHistory.removeAll()
+        updateResult("0")
+    }
+    
+    func handleChangeSign(currentResultText: String?) {
+        guard
+            let resultText = currentResultText,
+            var number = numberFormatter.number(from: resultText)?.doubleValue
+        else { return }
+        
+        number = -number
+        let formattedResult = (number.truncatingRemainder(dividingBy: 1) == 0) ? String(Int(number)) : numberFormatter.string(from: NSNumber(value: number))
+        updateResult(formattedResult ?? "0")
+    }
+    
+    private func updateResult(_ resultText: String) {
+        onResultUpdated?(resultText)
+    }
+    
+    private func updateHistory(_ historyText: String? = nil) {
+        let history = historyText ?? buildOperationHistory()
+        onHistoryUpdated?(history)
+    }
+    
+    private func buildOperationHistory() -> String {
+           var history = ""
+           for item in calculationHistory {
+               switch item {
+               case .number(let number):
+                   if let formattedNumber = numberFormatter.string(from: NSNumber(value: number)) {
+                       history += "\(formattedNumber) "
+                   }
+               case .operation(let operation):
+                   history += "\(operation.rawValue) "
+               }
+           }
+           return history
+       }
+    
+    private func calculate() throws -> Double {
+        guard case .number(let firstNumber) = calculationHistory[0] else { return 0 }
+        var currentResult = firstNumber
+        
+        for index in stride(from: 1, through: calculationHistory.count - 1, by: 2) {
+            guard
+                case .operation(let operation) = calculationHistory[index],
+                case .number(let number) = calculationHistory[index + 1]
+            else { break }
+            
+            currentResult = try operation.calculate(currentResult, number)
+        }
+        
+        return currentResult
+    }
+}
--- Another calculator/ViewController.swift
+++ Another calculator/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var operationHistory: UILabel!
+    
+    private let viewModel = CalculatorViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewModel.onResultUpdated = { [weak self] resultText in
+            self?.result.text = resultText
+        }
+        
+        viewModel.onHistoryUpdated = { [weak self] historyText in
+            self?.operationHistory.text = historyText
+        }
     }
-
-
+    
+    @IBAction func сhangeSignPressed(_ sender: UIButton) {
+        viewModel.handleChangeSign(currentResultText: result.text)
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        viewModel.handleClear()
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        viewModel.handleButtonPress(buttonText, currentResultText: result.text)
+    }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.accessibilityIdentifier else { return }
+        viewModel.handleOperationPress(buttonText, currentResultText: result.text)
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        viewModel.handleCalculatePress(currentResultText: result.text)
+    }
+    
 }
 
+class RoundedButton: UIButton {
+
+  
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupRoundedCorners()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupRoundedCorners()
+    }
+
+    private func setupRoundedCorners() {
+        self.layer.cornerRadius = self.frame.size.height / 2
+        self.layer.masksToBounds = true
+    }
+}
